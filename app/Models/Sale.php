c0783da9399b13cbcdac969a1be503a8299dd1b6
CRAFTDEATH <adriano1997r@hotmail.com>
2023-05-28 14:09:59 -0300

    first commit

---
+++ app/Models/Sale.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Sale extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'seller_id',
+        'client_id',
+        'sold_at',
+        'status',
+        'total_amount'
+    ];
+
+    protected $cast = [
+        'status' => Status::class
+    ];
+
+    public function client(): BelongsTo
+    {
+        return $this->belongsTo(Client::class);
+    }
+
+    public function seller(): BelongsTo
+    {
+        return $this->belongsTo(Seller::class);
+    }
+}
